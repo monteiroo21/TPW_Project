dba549d4331de3b49ca6492809c280abbb2114c5
Joao <joaomonteiro29@ua.pt>
2024-10-27 00:07:39 +0100

    setup of django project and insert main models

---
+++ CarStand/app/models.py
@@ -0,0 +1,29 @@
+from typing import Any
+from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+
+# Create your models here.
+class StandManager(UserManager):
+    def create_user(self, username, password):
+        pass
+    def create_superuser(self, username: str, email: str | None, password: str | None):
+        pass
+
+
+class Client(AbstractUser):
+    pass
+
+class Group(models.Model):
+    pass
+
+class Brand(models.Model):
+    pass
+
+class Models(models.Model):
+    pass
+
+class Car(models.Model):
+    pass
+
+class Moto(models.Model):
+    pass
