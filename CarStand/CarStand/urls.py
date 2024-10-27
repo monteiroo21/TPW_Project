3ad012f6ee5ebcd191736f3d9c622772ef783515
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-10-27 16:31:46 +0000

    redirect automactly for index/ url

--- CarStand/CarStand/urls.py
+++ CarStand/CarStand/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    # path('', views.index),
+    path('', RedirectView.as_view(url='index/', permanent=True)),
+    path('index/', views.index),
 ]
