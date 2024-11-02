100f13e8ac1e820aaf02634fa153170b95164287
joaoviegas11 <joaoviegas11@ua.pt>
2024-11-02 20:24:42 +0000

    add motos

--- CarStand/app/models.py
+++ CarStand/app/models.py
@@ -76,9 +76,16 @@ class Car(models.Model):
     new = models.BooleanField(default=True)
     kilometers = models.FloatField(default=0.0, validators=[MinValueValidator(0.0)])
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image=models.ImageField(upload_to='./static/imgs/cars')
 
     def __str__(self):
         return self.model.name + ", " + self.model.brand.name
 
-# class Moto(models.Model):
-#     pass
+class Moto(models.Model):
+    model = models.ForeignKey(CarModel, related_name='model_motos', on_delete=models.CASCADE)
+    year = models.PositiveIntegerField(validators=[MinValueValidator(1990), MaxValueValidator(2024)])
+    new = models.BooleanField(default=True)
+    kilometers = models.FloatField(default=0.0, validators=[MinValueValidator(0.0)])
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image=models.ImageField(upload_to='./static/imgs/motos')
+
