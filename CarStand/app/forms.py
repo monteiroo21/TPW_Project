912e5effcc769afcebf7970d304c381f39b6107d
Joao <joaomonteiro29@ua.pt>
2024-11-06 15:42:10 +0000

    refactor brands

--- CarStand/app/forms.py
+++ CarStand/app/forms.py
@@ -23,3 +23,8 @@ class LoginForm(forms.Form):
 class GroupSearchForm(forms.Form):
     query = forms.CharField(label="Search", max_length=100, required=False, widget=forms.TextInput(
         attrs={'class': 'w-80 pl-10 pr-4 py-2 border-2 border-sky-800 bg-white rounded-lg focus:outline-none focus:ring-2 focus:ring-sky-800', 'placeholder': ('Search for a group')}))
+
+
+class BrandSearchForm(forms.Form):
+    query = forms.CharField(label="Search", max_length=100, required=False, widget=forms.TextInput(
+        attrs={'class': 'w-80 pl-10 pr-4 py-2 border-2 border-sky-800 bg-white rounded-lg focus:outline-none focus:ring-2 focus:ring-sky-800', 'placeholder': ('Search for a brand')}))
