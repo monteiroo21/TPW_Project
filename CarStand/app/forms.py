3ffb0eff4048f8d6a858f66754419f44ae3cfa5a
joaoviegas11 <joaoviegas11@ua.pt>
2024-11-06 21:36:20 +0000

    mini change

--- CarStand/app/forms.py
+++ CarStand/app/forms.py
@@ -68,7 +68,7 @@ class CarSortAndFilter(forms.Form):
 
     newOrUsed = forms.ChoiceField(
         choices=[
-            ("All", "All cars"),
+            ("All", "New and Used"),
             ("true", "New Cars"),
             ("false", "Used cars")
         ],
@@ -79,13 +79,13 @@ class CarSortAndFilter(forms.Form):
     
     color = forms.ChoiceField(
         choices=[
-            ("None", "None"),
-            ("blue", "blue"),
-            ("red", "red"),
-            ("white", "white"),
-            ("black", "black"),
-            ("grey", "grey"),
-            ("green", "green")
+            ("None", "All colors"),
+            ("blue", "Blue"),
+            ("red", "Red"),
+            ("white", "White"),
+            ("black", "Black"),
+            ("grey", "Grey"),
+            ("green", "Green")
         ],
         widget=forms.Select(attrs={'class': 'form-control block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'}),
         label=''
@@ -129,9 +129,9 @@ class MotoSortAndFilter(forms.Form):
 
     newOrUsed = forms.ChoiceField(
         choices=[
-            ("All", "All cars"),
-            ("true", "New Cars"),
-            ("false", "Used cars")
+            ("All", "New and Used"),
+            ("true", "New motos"),
+            ("false", "Used motos")
         ],
         widget=forms.Select(attrs={'class': 'form-control block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'}),
         label=''
@@ -140,13 +140,13 @@ class MotoSortAndFilter(forms.Form):
     
     color = forms.ChoiceField(
         choices=[
-            ("None", "None"),
-            ("blue", "blue"),
-            ("red", "red"),
-            ("white", "white"),
-            ("black", "black"),
-            ("grey", "grey"),
-            ("green", "green")
+            ("None", "All colors"),
+            ("blue", "Blue"),
+            ("red", "Red"),
+            ("white", "White"),
+            ("black", "Black"),
+            ("grey", "Grey"),
+            ("green", "Green")
         ],
         widget=forms.Select(attrs={'class': 'form-control block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'}),
         label=''
