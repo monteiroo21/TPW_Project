5ea1e90055daffecb97b05ca9117746a30db84e8
Joao <joaomonteiro29@ua.pt>
2024-11-06 01:06:30 +0000

    .

--- CarStand/app/forms.py
+++ CarStand/app/forms.py
@@ -13,7 +13,15 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username', 'first_name', 'last_name',
 'email', 'password1', 'password2',)
-        
+
+
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+'''
+class GroupSearchForm(forms.Form):
+    query = forms.CharField(label="", max_length=50, required=False, widget=forms.TextInput(
+        attrs={'class': ' bg-transparent', 'placeholder': _('Search for a group')}))
+
+'''
