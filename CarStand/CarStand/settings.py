4358a1368d0975819363955f84b57404e7b46bb0
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-11-06 21:05:09 +0000

    data base changed again for good

--- CarStand/CarStand/settings.py
+++ CarStand/CarStand/settings.py
@@ -117,8 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'app/static') 
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'app/media')
+
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index' 
 
--- CarStand/CarStand/urls.py
+++ CarStand/CarStand/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from app import views
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -41,3 +43,5 @@ urlpatterns = [
     path('motorbike/<int:moto_id>/', views.motorbike_detail, name='motorbike_detail'),
     path('createCar/', views.createCar, name='createCar'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
