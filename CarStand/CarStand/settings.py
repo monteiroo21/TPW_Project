dc5e52ac4bb70f56084b96fdd3b73c46c2f32e74
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-12-03 16:22:48 +0000

    requirements.txt update

--- CarStand/CarStand/settings.py
+++ CarStand/CarStand/settings.py
@@ -38,12 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
+    'app.apps.AppConfig',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
