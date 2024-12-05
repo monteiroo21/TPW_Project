c9692efe243888905f414315e69e2aaa9b9f544c
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-12-05 21:46:11 +0000

    cars and motos cards updates

--- carStand-app/src/app/components/cars/cars.component.ts
+++ carStand-app/src/app/components/cars/cars.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CardsAndMotosCardsComponent } from '../Cards/cards-and-motos-cards/cards-and-motos-cards.component';
 
 @Component({
   selector: 'app-cars',
-  imports: [],
+  imports: [CardsAndMotosCardsComponent],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
 })
