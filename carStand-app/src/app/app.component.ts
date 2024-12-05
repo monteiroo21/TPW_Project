36d02a710009374103994d06a0550df1e02c054c
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-12-05 20:24:25 +0000

    footer component

--- carStand-app/src/app/app.component.ts
+++ carStand-app/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
