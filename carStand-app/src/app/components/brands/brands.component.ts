1b12da87c19336d73ee0b5186a79687d956b4ad9
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-12-06 16:45:33 +0000

    now all the pages have cards

--- carStand-app/src/app/components/brands/brands.component.ts
+++ carStand-app/src/app/components/brands/brands.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BrandsAndGroupsCardsComponent } from '../Cards/brands-and-groups-cards/brands-and-groups-cards.component';
 
 @Component({
   selector: 'app-brands',
-  imports: [],
+  imports: [BrandsAndGroupsCardsComponent],
   templateUrl: './brands.component.html',
   styleUrl: './brands.component.css'
 })
--- carStand-app/src/app/components/motor-bikes/motor-bikes.component.ts
+++ carStand-app/src/app/components/motor-bikes/motor-bikes.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CardsAndMotosCardsComponent } from '../Cards/cards-and-motos-cards/cards-and-motos-cards.component';
 
 @Component({
   selector: 'app-motor-bikes',
-  imports: [],
+  imports: [CardsAndMotosCardsComponent],
   templateUrl: './motor-bikes.component.html',
   styleUrl: './motor-bikes.component.css'
 })
