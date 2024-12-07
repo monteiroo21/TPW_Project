157a3286ef93e0dcee17e072111c3ae71950ab1d
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-12-07 16:20:41 +0000

    carousel component

---
+++ carStand-app/src/app/components/home-carousel/home-carousel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home-carousel',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './home-carousel.component.html',
+  styleUrl: './home-carousel.component.css'
+})
+export class HomeCarouselComponent {
+
+}
--- carStand-app/src/app/components/home/home.component.ts
+++ carStand-app/src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HomeCarouselComponent } from '../home-carousel/home-carousel.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, HomeCarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
