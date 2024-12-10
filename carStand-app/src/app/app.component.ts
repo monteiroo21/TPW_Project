9c10b1dc84dbb80fa9fefbbed66d74edbf0da616
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-12-10 14:50:26 +0000

    login without navbar

--- carStand-app/src/app/app.component.ts
+++ carStand-app/src/app/app.component.ts
@@ -1,21 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'], // Corrigido: styleUrls no plural
 })
 export class AppComponent implements OnInit {
   title = 'carStand-app';
+  showNavbarFooter = true;
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     initFlowbite();
+
+    this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const currentRoute = event.urlAfterRedirects.split('?')[0];
+        const excludedRoutes = ['/login'];
+
+        this.showNavbarFooter = !excludedRoutes.includes(currentRoute);
+      });
   }
 }
