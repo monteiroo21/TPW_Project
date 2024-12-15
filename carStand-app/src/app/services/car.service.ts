898dad044e7e6311090e21479adf6e1cf9e678a8
joaoviegas11 <joaoviegas11@ua.pt>
2024-12-15 15:29:34 +0000

    create and edit v not work and delete work

--- carStand-app/src/app/services/car.service.ts
+++ carStand-app/src/app/services/car.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Car } from '../interfaces/car';
+import { CarModel } from '../interfaces/model';
 
 @Injectable({
   providedIn: 'root'
@@ -27,5 +28,52 @@ export class CarService {
     return await data.json() ?? [];
   }
 
+  async createCar(car: any): Promise<any> {
+    const url = `${this.baseURL}/cars/create`;
+    const response = await fetch(url, {
+      method: 'POST',
+
+      body: car,
+    });
+    return await response.json();
+  }
+
+  async getModelsByType(vehicleType: string): Promise<CarModel[]> {
+    const url = `${this.baseURL}/models/${vehicleType}/`;
+    try {
+      const response = await fetch(url);
+
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+
+  async deleteCar(id: number): Promise<void> {
+    const url = `${this.baseURL}/cars/delete/${id}`;
+    const response = await fetch(url, {
+      method: 'DELETE',
+    });
+  
+    if (!response.ok) {
+      throw new Error('Failed to delete car');
+    }
+  }
+  
+  async updateCar(car: Car): Promise<void> {
+    const url = `${this.baseURL}/cars/update`;
+    console.log(car);
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(car),
+    });
+  
+    if (!response.ok) {
+      throw new Error('Failed to update car');
+    }
+  }
+  
   // Depois implementar o resto dos métodos
 }
