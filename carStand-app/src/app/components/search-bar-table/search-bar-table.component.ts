52e72705c2f94783b360bc5f31a9b9413656534f
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-12-16 12:36:44 +0000

    search manager table

---
+++ carStand-app/src/app/components/search-bar-table/search-bar-table.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar-table',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './search-bar-table.component.html',
+  styleUrl: './search-bar-table.component.css'
+})
+export class SearchBarTableComponent {
+  queryVehicle: string = '';
+  queryUser: string = '';
+
+  @Output() searchFilters = new EventEmitter<{ queryVehicle: string; queryUser: string }>();
+
+  // Emite os filtros para o componente pai
+  emitFilters(): void {
+    this.searchFilters.emit({
+      queryVehicle: this.queryVehicle,
+      queryUser: this.queryUser
+    });
+  }
+}
--- carStand-app/src/app/services/purchaser-and-selected-vehicles.service.ts
+++ carStand-app/src/app/services/purchaser-and-selected-vehicles.service.ts
@@ -8,13 +8,13 @@ export class PurchaserAndSelectedVehiclesService {
 
   private baseURL = 'http://localhost:8000/api/';
 
-  constructor() {}
+  constructor() { }
 
   async toggleInterest(vehicleId: number, type: string): Promise<any> {
     const url = `${this.baseURL}vehicles/${vehicleId}/${type}/interest/`;
     try {
       const response = await fetch(url, {
-        method: 'POST', 
+        method: 'POST',
         headers: new Headers({
           'Content-Type': 'application/json',
           'Accept': 'application/json',
@@ -29,7 +29,7 @@ export class PurchaserAndSelectedVehiclesService {
   async approveCustomer(vehicleId: number, profile_id: number, type: string): Promise<any> {
     const url = `${this.baseURL}vehicles/${vehicleId}/${profile_id}/${type}/approve/`;
     try {
-      const response = await fetch(url,{ method: 'POST'});
+      const response = await fetch(url, { method: 'POST' });
 
       return await response.json();
     } catch (error) {
@@ -37,46 +37,46 @@ export class PurchaserAndSelectedVehiclesService {
     }
   }
 
-  async negateCustomer(vehicleId: number,  profile_id: number, type: string): Promise<any> {
+  async negateCustomer(vehicleId: number, profile_id: number, type: string): Promise<any> {
     const url = `${this.baseURL}vehicles/${vehicleId}/${profile_id}/${type}/negate/`;
     try {
-      const response = await fetch(url,{ method: 'POST'});
+      const response = await fetch(url, { method: 'POST' });
 
       return await response.json();
     } catch (error) {
       console.error(error);
     }
   }
-  async getVehicleStatus(vehicleId: number, type: string): Promise<{ isSelected: boolean; isBuyed: boolean | null}> {
+  async getVehicleStatus(vehicleId: number, type: string): Promise<{ isSelected: boolean; isBuyed: boolean | null }> {
     const url = `${this.baseURL}vehicles/${vehicleId}/${type}/status/`;
-  
+
     try {
       const response = await fetch(url);
-  
+
       return await response.json();
     } catch (error) {
       console.error(error);
-      return { isSelected: false, isBuyed: null }; 
+      return { isSelected: false, isBuyed: null };
     }
   }
-  async getVehiclesForApproval(): Promise<{ listForAccept: ElementForAccept[] }> {
-    const url = `${this.baseURL}vehicles/approval/`;
-    // if (filterProfile) {
-    //   url.searchParams.append('filterProfile', 'true');
-    // }
-  
+  async getVehiclesForApproval(queryVehicle: string = '', queryUser: string = ''): Promise<{ listForAccept: ElementForAccept[] }> {
+    const url = new URL(`${this.baseURL}vehicles/approval/`);
+
+    if (queryVehicle) url.searchParams.append('qVehicle', queryVehicle);
+    if (queryUser) url.searchParams.append('qUser', queryUser);
+
     try {
       const response = await fetch(url);
-  
+
       if (!response.ok) {
         throw new Error(`Failed to fetch vehicles for approval: ${response.statusText}`);
       }
-  
+
       return await response.json();
     } catch (error) {
       console.error(error);
       return { listForAccept: [] };
     }
   }
-  
+
 }
