244dc15e2dfc32923652f1e6611d8314071eb709
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-12-17 23:43:49 +0000

    environment.ts

--- carStand-app/src/app/environments/environment.ts
+++ carStand-app/src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    apiBaseUrl: 'https://joaov2345678.pythonanywhere.com',
-    apiBaseUrlApi: 'https://joaov2345678.pythonanywhere.com/api',
-  };
-  
+  // apiBaseUrl: 'https://joaov2345678.pythonanywhere.com',
+  // apiBaseUrlApi: 'https://joaov2345678.pythonanywhere.com/api',
+  apiBaseUrl: 'http://localhost:8000',
+  apiBaseUrlApi: 'http://localhost:8000/api',
+};
